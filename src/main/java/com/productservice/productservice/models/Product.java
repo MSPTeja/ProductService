70389acf85744d8d3893b8e3ec6176b2bca05f2a
TEJA <mspteja722@gmail.com>
2023-12-28 10:49:57 +0530

    DB Connectivity

--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,15 +1,20 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
 
     private String title;
-    private String desc;
+    private String description;
     private int price;
     private String image;
+    // Category is not primitive datatype so need to map.
+    @ManyToOne
     private Category category;
 }
